2178aa42356260a506fe943d46777bb9055336a7
gdgfjhihch-boop <gdgfjhihch@gmail.com>
2026-02-20 18:24:27 +0530

    Refactor Babel config to use react-native-worklets-core

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,10 @@
 module.exports = function (api) {
   api.cache(true);
-  let plugins = [];
-
-  plugins.push("react-native-worklets/plugin");
-
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"],
-    plugins,
+    presets: ['babel-preset-expo'],
+    plugins: [
+      // पक्का करें कि यहाँ '-core' लिखा है, यही उस 'लाल स्क्रीन' का इलाज है
+      'react-native-worklets-core/plugin',
+    ],
   };
 };
